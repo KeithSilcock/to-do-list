83a99d3141e871302ab964d3c379eccf7a2c58f2
KeithSilcock <silcockk@gmail.com>
2018-05-02 16:22:02 -0700

    displayed dummy data on html

---
+++ src/helpers/list_data.js
@@ -0,0 +1,23 @@
+
+export default [
+    {
+        title:'Learn how to React',
+        details:'I\'ve always wanted to understand how people react to things',
+    },
+    {
+        title:'Learn how to feel',
+        details:'I\'ve always wanted to understand how people feel things',
+    },
+    {
+        title:'Learn how to eat',
+        details:'I\'ve always wanted to understand how people eat things',
+    },
+    {
+        title:'Learn how to run',
+        details:'I\'ve always wanted to understand how people run around things',
+    },
+    {
+        title:'Learn how to push in my chair',
+        details:'Sam is really annoyed that I don\'t take care of my shit',
+    },
+];
